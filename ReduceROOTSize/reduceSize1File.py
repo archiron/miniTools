b42092e666855effcc11d9299215c18a0472c439
archiron <arnaud.chiron@cern.ch>
2026-07-09 11:57:10 +0200

    Version 2026.07.09-01
    add root file name into red bold.

--- ReduceROOTSize/reduceSize1File.py
+++ ReduceROOTSize/reduceSize1File.py
@@ -28,6 +28,35 @@ from ROOT import TFile
 
 sys.path.append('../ChiLib')
 
+def changeColor(color):
+    # 30:noir ; 31:rouge; 32:vert; 33:orange; 34:bleu; 35:violet; 36:turquoise; 37:blanc
+    # other references at https://misc.flogisoft.com/bash/tip_colors_and_formatting
+    if (color == 'black'):
+        return '[30m'
+    elif (color == 'red'):
+        return '[31m'
+    elif (color == 'b_red'): # BOLD
+        return '[1;31m'
+    elif (color == 'green'):
+        return '[32m'
+    elif (color == 'orange'):
+        return '[33m'
+    elif (color == 'blue'):
+        return '[34m'
+    elif (color == ''):
+        return '[35m'
+    elif (color == 'purple'):
+        return '[36m'
+    elif (color == 'turquoise'):
+        return '[37m'
+    elif (color == 'lightyellow'):
+        return '[93m'
+    else:
+        return '[30m'
+
+def colorText(sometext, color):
+    return '\033' + changeColor(color) + sometext + '\033[0m'
+
 def changeDirectory(rootFile, path):
     """
     Change the current directory (ROOT.gDirectory) by the corresponding (rootFile,pathSplit)
@@ -85,8 +114,10 @@ input_file = sys.argv[1] # '/DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO_' +
 racine = input_file.split('.root')
 output_file = racine[0] + 'b.root' # + racine[1]
 
-print('\n %s' % input_file)
-print('\n %s' % output_file)
+#print('\n %s' % input_file)
+#print('\n %s' % output_file)
+print('\n {:s}'.format(colorText(input_file, 'b_red')))
+print('\n {:s}'.format(colorText(output_file, 'b_red')))
 
 paths = ['DQMData/Run 1/EgammaV', 'DQMData/Run 1/Info']
 
